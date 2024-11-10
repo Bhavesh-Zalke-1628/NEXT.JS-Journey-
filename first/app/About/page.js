5dd84c31bb17930a9e18d09f3dd538f6498af054
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-11-10 16:28:03 +0530

    Start my next journey with initial commit

---
+++ first/app/About/page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1 className=' text-xl '> problem solved by next</h1>
+            <ul>
+                <li>
+                    Full stack solution
+                </li>
+                <li>
+                    file base routing
+                </li>
+                <li>additional router feature</li>
+                <li>optimize rendring </li>
+            </ul>
+            i am about
+        </div>
+    )
+}
+
+export default page
